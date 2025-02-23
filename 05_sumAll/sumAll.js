dd440905d54a74f78030ca3b77f127f4b6ba2ed9
Gabriel Molina <gabrielmolina717@gmail.com>
2025-02-23 22:40:07 +0800

    added solution for 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function (...given) {
+  let finalSum = 0;
 
+  let sortedGiven = given.sort();
+  const firstValue = sortedGiven[0];
+  const secondValue = sortedGiven[1];
+
+  if (
+    Number.isInteger(firstValue) &&
+    Number.isInteger(secondValue) &&
+    firstValue > 0 &&
+    secondValue > 0
+  ) {
+    for (let i = firstValue; i <= secondValue; i++) {
+      finalSum += i;
+    }
+
+    return finalSum;
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
