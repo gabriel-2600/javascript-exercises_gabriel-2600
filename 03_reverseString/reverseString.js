1e2046af55a1f8a7a912eab8c4cb3c29a590e64b
Gabriel Molina <gabrielmolina717@gmail.com>
2025-02-23 12:58:17 +0800

    Solution for 03_reverseString all 4 test cases passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  let word = "";
 
+  for (let i = string.length - 1; i >= 0; i--) {
+    word += string.charAt(i);
+  }
+
+  return word;
 };
 
 // Do not edit below this line
