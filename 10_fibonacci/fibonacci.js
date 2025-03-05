0f5a506b397744d358661906a0bb1bfa0f98f4fd
Gabriel Molina <gabrielmolina717@gmail.com>
2025-03-05 20:09:43 +0800

    added solution for 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  const convertedNum = Number(num);
+  if (convertedNum < 0) {
+    return "OOPS";
+  } else if (convertedNum === 0) {
+    return 0;
+  }
 
+  let firstPrev = 1;
+  let secondPrev = 0;
+
+  for (let i = 2; i <= convertedNum; i++) {
+    let current = firstPrev + secondPrev;
+    secondPrev = firstPrev;
+    firstPrev = current;
+  }
+
+  return firstPrev;
 };
 
 // Do not edit below this line
