e1196b2772d661d2143759cd78f057b3e957b13b
Gabriel Molina <gabrielmolina717@gmail.com>
2025-03-05 17:34:10 +0800

    Added solution for 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function (numOne, numTwo) {
+  return Number(numOne) + Number(numTwo);
 };
 
-const subtract = function() {
-	
+const subtract = function (numOne, numTwo) {
+  return Number(numOne) - Number(numTwo);
 };
 
-const sum = function() {
-	
+const sum = function (numArray) {
+  if (numArray.length === 0) {
+    return 0;
+  }
+  return numArray.reduce(
+    (accumulator, currentValue) => accumulator + currentValue
+  );
 };
 
-const multiply = function() {
-
+const multiply = function (numArray) {
+  return numArray.reduce(
+    (accumulator, currentValue) => accumulator * currentValue
+  );
 };
 
-const power = function() {
-	
+const power = function (numOne, numTwo) {
+  return Math.pow(numOne, numTwo);
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num === 0) return 1;
+
+  let total = 1;
+  for (let i = 1; i <= num; i++) {
+    total *= i;
+  }
+
+  return total;
 };
 
 // Do not edit below this line
@@ -29,5 +43,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
