47888bcee7d0fc7cfeb1a996f24a951f99f5b4dc
Gabriel Molina <gabrielmolina717@gmail.com>
2025-03-05 18:26:38 +0800

    Added solution for 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (word) {
+  const punctuation = /[\.,?!]/g;
+  const noPunctuation = word.replace(punctuation, "");
+  const noSpaceAndLowerCase = noPunctuation.split(" ").join("").toLowerCase();
 
+  const charArray = noSpaceAndLowerCase.split("");
+  const reversedCharArray = charArray.toReversed();
+
+  for (let i = 0; i < charArray.length; i++) {
+    if (charArray[i] !== reversedCharArray[i]) {
+      return false;
+    }
+  }
+  return true;
 };
 
 // Do not edit below this line
